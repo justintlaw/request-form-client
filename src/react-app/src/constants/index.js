4279ee448ce148e646170f5af4ccb03e963a6f3d
Justin Lawrence <justintlaw98@gmail.com>
2022-04-12 00:38:25 -0600

    update url

--- src/react-app/src/constants/index.js
+++ src/react-app/src/constants/index.js
@@ -1,5 +1,5 @@
 /* PROD URL */
-// export const API_BASE_URL = 'https://6n518dbwi5.execute-api.us-west-2.amazonaws.com/v1/api'
+export const API_BASE_URL = 'https://6n518dbwi5.execute-api.us-west-2.amazonaws.com/v1/api'
 
 /* LOCAL URL */
-export const API_BASE_URL = 'http://localhost:3001/api'
+// export const API_BASE_URL = 'http://localhost:3001/api'
