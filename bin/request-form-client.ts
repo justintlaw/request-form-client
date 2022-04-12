7688eca7ff59bf99f4fc005ea149cb41b6b8c6be
Justin Lawrence <justintlaw98@gmail.com>
2022-04-11 18:23:24 -0600

    add connection arn for codestar

--- bin/request-form-client.ts
+++ bin/request-form-client.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { RequestFormClientStack } from '../lib/request-form-client-stack';
+import { RequestFormClientPipelineStack } from '../lib/request-form-client-pipeline';
 
 const app = new cdk.App();
-new RequestFormClientStack(app, 'RequestFormClientCodePipeline', {
+new RequestFormClientPipelineStack(app, 'RequestFormClientCodePipeline', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/request-form-client-pipeline.ts
+++ lib/request-form-client-pipeline.ts
@@ -9,7 +9,7 @@ export class RequestFormClientPipelineStack extends Stack {
     const pipeline = new pipelines.CodePipeline(this, 'FrontendPipeline', {
       synth: new pipelines.ShellStep('Synth', {
         input: pipelines.CodePipelineSource.connection('justintlaw/request-form-client', 'main', {
-          connectionArn: 'GOES HERE' // TODO
+          connectionArn: 'arn:aws:codestar-connections:us-west-2:256343118501:connection/140bfd92-70b8-474c-bc79-42c432c97c0c'
         }),
         commands: [
           'cd src/react-app && npm install --production',
